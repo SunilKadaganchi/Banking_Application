36c3195c11ecda7d1e22b0bfd8cacd8e230df7ad
SunilKadaganchi <sunilkadaganchi071@gmail.com>
2025-04-08 10:59:19 +0530

    adding banking and notification microservices

---
+++ notification-micro/src/main/java/com/bankApp/notification_micro/NotificationMicroApplication.java
@@ -0,0 +1,13 @@
+package com.bankApp.notification_micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationMicroApplication.class, args);
+	}
+
+}
